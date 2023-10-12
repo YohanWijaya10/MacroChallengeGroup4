3b28941b6c8166e29f77a3a95dea4be5dec493c2
Nam Do-San <bputrantio@student.ciputra.ac.id>
2023-10-12 15:37:17 +0700

    Initial Commit

---
+++ MacroChallengeGroup4/MacroChallengeGroup4App.swift
@@ -0,0 +1,17 @@
+//
+//  MacroChallengeGroup4App.swift
+//  MacroChallengeGroup4
+//
+//  Created by Brian Putrantio on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MacroChallengeGroup4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
