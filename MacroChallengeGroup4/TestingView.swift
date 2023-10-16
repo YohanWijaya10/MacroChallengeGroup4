7b8929e557dc3ae80b69f49711aac378c4829b75
Nam Do-San <bputrantio@student.ciputra.ac.id>
2023-10-16 14:32:28 +0700

    Bikin testingView

--- MacroChallengeGroup4/TestingView.swift
+++ MacroChallengeGroup4/TestingView.swift
@@ -1,8 +1,54 @@
 //
 //  TestingView.swift
-//  MacroChallengeGroup4
+//  Macro_coba1
 //
-//  Created by Brian Putrantio on 16/10/23.
+//  Created by Brian Putrantio on 14/10/23.
 //
 
 import Foundation
+import SwiftUI
+
+struct TestingView: View {
+    var body: some View {
+        ZStack {
+            Color.yellow.edgesIgnoringSafeArea(.all)
+            
+            HStack {
+                Image("chair")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 300)
+                
+                VStack{
+                    HStack{
+                        Text("Sit Properly")
+                            .font(.largeTitle)
+                            .bold()
+                            .foregroundColor(.blue)
+                        
+                        Text("in the chair")
+                            .font(.largeTitle)
+                            .foregroundColor(.blue)
+                    }
+                    Button(action: {
+                        
+                    }) {
+                        Text("Next")
+                            .font(.headline)
+                            .padding()
+                            .background(Color.cyan)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                }
+            }
+            Spacer()
+        }
+    }
+}
+
+struct TestingView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestingView()
+    }
+}
