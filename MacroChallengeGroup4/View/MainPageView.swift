10dce3993dbc7f7e762249b9eec713aebb2a5d46
Nam Do-San <bputrantio@student.ciputra.ac.id>
2023-10-30 14:22:50 +0700

    Update MainPage(Add profile image, setting button)

--- MacroChallengeGroup4/View/MainPageView.swift
+++ MacroChallengeGroup4/View/MainPageView.swift
@@ -9,14 +9,26 @@ import Foundation
 import SwiftUI
 
 struct MainPageView: View{
-    var NamaUser: String = "Osama Blink Dagger"
+    var NamaUser: String = "Nadine"
     @State private var Freeplay_isPressed = false
     @State private var Songs_isPressed = false
     @State private var Courses_isPressed = false
     
     var body: some View{
         ZStack{
+            Color("Papyrus")
+                .edgesIgnoringSafeArea(.all)
+            
             ZStack{
+                VStack{
+                    Rectangle().roundedCorner(50, corners: [.bottomLeft, .bottomRight])
+                        .edgesIgnoringSafeArea(.horizontal)
+                        .frame(width: 800, height: 120)
+                        .foregroundColor(Color("Yellow"))
+                    
+                    Spacer()
+                }
+
                 VStack{
                     HStack {
                         Image(systemName:  "person.crop.circle.fill") // Profile Image
@@ -30,19 +42,26 @@ struct MainPageView: View{
                         
                         Spacer()
                         
-                        Image("Help_Icons") // Button ?
-                            .resizable()
-                            .frame(width: 90, height: 90)
-                            .padding()
-                        Image("Setting_Icon") // Button Setting
-                            .resizable()
-                            .frame(width: 60, height: 60)
-                            .padding()
+                        Button(action: {
+                            withAnimation {
+                                
+                            }
+                        }) {
+                            Image(systemName: "gearshape.fill")
+                                .font(.title)
+                                .frame(width: 20, height: 20)
+                                .padding()
+                                .background(Color("Navy"))
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                                .padding(30)
+                        }
                     }
                     .padding(.top, 5)
                     .padding(.leading, 5)
                     Spacer()
-                }.foregroundColor(.yellow)
+                }
+                .foregroundColor(Color("Papyrus"))
             }
             Spacer()
             
@@ -63,12 +82,7 @@ struct MainPageView: View{
                             .offset(y: Freeplay_isPressed ? 5 : 0) // Menggeser tombol ke bawah saat ditekan
                             .padding(30)
                     }
-                    
-                    /*Image("chair") // Button Courses
-                     .resizable()
-                     .frame(width: 150, height: 100)
-                     .padding(30)*/
-                    
+                                        
                     Button(action: {
                         withAnimation {
                             Courses_isPressed.toggle()
@@ -78,7 +92,7 @@ struct MainPageView: View{
                             .font(.title)
                             .frame(width: 150, height: 100)
                             .padding()
-                            .background(Courses_isPressed ? Color.blue : Color.green)
+                            .background(Courses_isPressed ? Color.green : Color.green)
                             .foregroundColor(.white)
                             .cornerRadius(10)
                             .offset(y: Courses_isPressed ? 5 : 0) // Menggeser tombol ke bawah saat ditekan
