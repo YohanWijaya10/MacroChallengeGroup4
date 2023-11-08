c6d8ce81f1645ce83e0097dea2e893c0af0f0166
Nam Do-San <bputrantio@student.ciputra.ac.id>
2023-11-08 14:17:05 +0700

    CREATE SONGPLAYVIEW, LEARNINGPLAYVIEW

--- MacroChallengeGroup4/View/CoursesPlayView.swift
+++ MacroChallengeGroup4/View/CoursesPlayView.swift
@@ -8,10 +8,97 @@
 import SwiftUI
 
 struct SongLearningView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+    var body: some View{
+        ZStack{
+            Color(Color("Yellow"))
+                .edgesIgnoringSafeArea(.all)
+            
+            ZStack{
+                ///Bagian Atas(Icon, Judul, Pause Button
+                VStack{
+                    HStack{
+                        ///Logo Icon
+                        ZStack{
+                            Circle()
+                            
+                            Image("Dummy Icon")
+                                .resizable()
+                                .scaledToFit()
+                            
+                            Image("Courses Icon Frame")
+                                .resizable()
+                                .scaledToFit()
+                                .foregroundColor(Color("Pink"))
+                            
+                            Text("Pattern 1")
+                                .font(.system(size: 20))
+                                .bold()
+                                .padding(.top, 65)
+                        }
+                        .frame(width: 120)
+                        .foregroundColor(Color("White"))
+                        
+                        ///Tulisan Pattern
+                        VStack{
+                            HStack{
+                                Text("Pattern 1")
+                                    .font(.largeTitle)
+                                    .bold()
+                                Spacer()
+                            }
+                            
+                            Rectangle()
+                                .frame(width: 200, height: 2)
+                            
+                            HStack{
+                                Text("Courses")
+                                Spacer()
+                            }
+                        }
+                        .frame(width: 200)
+                        .foregroundColor(Color("Navy"))
+                        
+                        Spacer()
+                        
+                        ///Button Pause
+                        Button(action: {
+                            
+                        }) {
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 10)
+                                    .frame(width: 40, height: 40)
+                                    .foregroundColor(Color("Navy"))
+                                
+                                Image(systemName:  "pause.fill")
+                                    .resizable()
+                                    .frame(width: 20, height: 25)
+                                    .foregroundColor(Color("Papyrus"))
+                            }
+                        }
+                    }
+                    
+                    Spacer()
+                        .frame(height: 200)
+                }
+                
+                ///Bagian Bawah(space buat tampilan play nya)
+                VStack{
+                    Spacer()
+                        .frame(height: 150)
+                    
+                    HStack{
+                        Spacer()
+                        
+                        Rectangle().roundedCorner(45, corners: [.bottomLeft, .topLeft])
+                            .edgesIgnoringSafeArea(.trailing)
+                            .frame(width: 700, height: 170)
+                            .foregroundColor(Color("Papyrus"))
+                    }
+                }
+            }
+            
+        }
+    }}
 
 #Preview {
     SongLearningView()
--- MacroChallengeGroup4/View/SongPlayView.swift
+++ MacroChallengeGroup4/View/SongPlayView.swift
@@ -5,21 +5,170 @@
 //  Created by Timothyus Kevin Dewanto on 30/10/23.
 //
 
+import Foundation
 import SwiftUI
 
-struct SongPlayView: View {
-    var body: some View {
-        VStack(){
-            HStack(){
+struct SongPlayView: View{
+    
+    var SongName: String = "SUPER SHY"
+    var SingerName: String = "New Jeans"
+    var Genre: String = "K-POP"
+    
+    @State var Metronome: String = "OFF"
+    @State var Speed: String = "1x"
+    
+    @State private var isMetronomOn = false
+    @State private var isSpeedOn = false
+    
+    var body: some View{
+        ZStack{
+            Color(Color("Yellow"))
+                .edgesIgnoringSafeArea(.all)
+            
+            ZStack{
+                ///Bagian Atas(Icon, Judul, Pause Button
+                VStack{
+                    HStack{
+                        ///Gambar Lagu
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 10)
+                            
+                            Image(systemName:  "photo") // Profile Image
+                                .resizable()
+                                .frame(width: 60, height: 60)
+                                .foregroundColor(Color("Papyrus"))
+                        }
+                        .frame(width: 100, height: 100)
+                        .foregroundColor(Color("Navy"))
+                        
+                        ///Tulisan Judul Lagu + Penyanyi
+                        VStack{
+                            HStack{
+                                Text(SongName)
+                                    .font(.title)
+                                    .bold()
+                                Spacer()
+                            }
+                            
+                            Rectangle()
+                                .frame(width: 300, height: 2)
+                            
+                            HStack{
+                                Text(SingerName)
+                                Spacer()
+                            }
+                        }
+                        .frame(width: 300)
+                        .foregroundColor(Color("Navy"))
+                        
+                        Spacer()
+                        
+                        ///Button Metronom
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(width: 150, height: 40)
+                            
+                            HStack{
+                                Text("Metronome")
+                                    .foregroundColor(Color("Navy"))
+                                
+                                Button(action: {
+                                    if isMetronomOn == false{
+                                        isMetronomOn = true
+                                        print(isMetronomOn)
+                                        Metronome = "ON"
+                                    }
+                                    else{
+                                        isMetronomOn = false
+                                        print(isMetronomOn)
+                                        Metronome = "OFF"
+                                    }
+                                }) {
+                                    ZStack{
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .frame(width: 40, height: 35)
+                                            .foregroundColor(Color("Navy"))
+                                        
+                                        Text(Metronome)
+                                    }
+                                }
+                            }
+                        }
+                        .frame(width: 150, height: 40)
+                        .foregroundColor(Color("Papyrus"))
+                        
+                        ///Button Speed
+                        ZStack{
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(width: 130, height: 40)
+                            
+                            HStack{
+                                Text("Speed")
+                                    .foregroundColor(Color("Navy"))
+                                
+                                Button(action: {
+                                    if isSpeedOn == false{
+                                        isSpeedOn = true
+                                        print(isSpeedOn)
+                                        Speed = "0,75x"
+                                    }
+                                    else{
+                                        isSpeedOn = false
+                                        print(isSpeedOn)
+                                        Speed = "1x"
+                                    }
+                                }) {
+                                    ZStack{
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .frame(width: 55, height: 35)
+                                            .foregroundColor(Color("Navy"))
+                                        
+                                        Text(Speed)
+                                    }
+                                }
+                            }
+                        }
+                        .frame(width: 130, height: 40)
+                        .foregroundColor(Color("Papyrus"))
+                        
+                        ///Button Pause
+                        Button(action: {
+                            
+                        }) {
+                            ZStack{
+                                RoundedRectangle(cornerRadius: 10)
+                                    .frame(width: 40, height: 40)
+                                    .foregroundColor(Color("Navy"))
+                                
+                                Image(systemName:  "pause.fill")
+                                    .resizable()
+                                    .frame(width: 20, height: 25)
+                                    .foregroundColor(Color("Papyrus"))
+                            }
+                            .frame(width: 40, height: 40)
+                        }
+                    }
+                    
+                    Spacer()
+                        .frame(height: 200)
+                }
                 
+                ///Bagian Bawah(space buat tampilan play nya)
+                VStack{
+                    Spacer()
+                        .frame(height: 150)
+                    
+                    HStack{
+                        Spacer()
+                        
+                        Rectangle().roundedCorner(45, corners: [.bottomLeft, .topLeft])
+                            .edgesIgnoringSafeArea(.trailing)
+                            .frame(width: 700, height: 170)
+                            .foregroundColor(Color("Papyrus"))
+                    }
+                }
             }
-        }
-        VStack(){
-            ZStack(){
-                VStack{}
-                VStack{}
-                VStack{}
-            }
+            
         }
     }
 }
@@ -27,3 +176,4 @@ struct SongPlayView: View {
 #Preview {
     SongPlayView()
 }
+
